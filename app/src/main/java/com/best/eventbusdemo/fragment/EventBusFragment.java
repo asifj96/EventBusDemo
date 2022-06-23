4e665ac8d38bdd051d2287e12413de278e16e509
asifj96 <masifj1996@gmail.com>
2022-06-22 22:45:28 -0700

    handling of custom Spinner

--- app/src/main/java/com/best/eventbusdemo/fragment/EventBusFragment.java
+++ app/src/main/java/com/best/eventbusdemo/fragment/EventBusFragment.java
@@ -1,11 +1,18 @@
 package com.best.eventbusdemo.fragment;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.fragment.app.Fragment;
 
 import com.best.eventbusdemo.R;
@@ -15,7 +22,12 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
+
 public class EventBusFragment extends Fragment {
+    private ArrayAdapter<String> spinnerAdapter;
+    ArrayList<String> spinnerList = new ArrayList<>();
+    private AppCompatSpinner spinner;
 
     public EventBusFragment() {
         // Required empty public constructor
@@ -26,6 +38,32 @@ public class EventBusFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_event_bus, container, false);
+        spinner = view.findViewById(R.id.spinner);
+        spinnerList.add("--Please Select Bundle--");
+        spinnerList.add("1");
+        spinnerList.add("2");
+        spinnerList.add("3");
+        spinnerAdapter = new ArrayAdapter<>
+                (getContext(), android.R.layout.simple_spinner_item, spinnerList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if(parent.getSelectedItemPosition() != 0){
+
+                    Toast.makeText(requireActivity(),"posss",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         return view;
